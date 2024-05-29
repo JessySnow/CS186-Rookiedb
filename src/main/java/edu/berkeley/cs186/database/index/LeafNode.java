0511054b2fb804178705ec5fdf6bedc5e2dee619
JessySnow <jessysnow12345@gmail.com>
2024-05-29 08:09:18 +0800

    feat: implement LeafNode::fromBytes

--- src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
+++ src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
@@ -376,8 +376,24 @@ class LeafNode extends BPlusNode {
         // Note: LeafNode has two constructors. To implement fromBytes be sure to
         // use the constructor that reuses an existing page instead of fetching a
         // brand new one.
+        Page page = bufferManager.fetchPage(treeContext, pageNum);
+        Buffer buf = page.getBuffer();
 
-        return null;
+        byte nodeType = buf.get();
+        assert(nodeType == (byte) 1);
+
+        long rightSibling = buf.getLong();
+        int nPairs = buf.getInt();
+
+        List<DataBox> keys = new ArrayList<>(nPairs);
+        List<RecordId> rids = new ArrayList<>(nPairs);
+
+        for (int i = 0; i < nPairs; ++i) {
+            keys.add(DataBox.fromBytes(buf, metadata.getKeySchema()));
+            rids.add(RecordId.fromBytes(buf));
+        }
+
+        return new LeafNode(metadata, bufferManager, page, keys, rids, Optional.of(rightSibling), treeContext);
     }
 
     // Builtins ////////////////////////////////////////////////////////////////
